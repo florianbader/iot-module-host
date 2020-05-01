404db68752d5c318970a211da442b555614e1d3c
Florian Bader <me@florian-bader.de>
2020-05-01 15:46:45 +0200

    Adds message and method handler bases

---
+++ src/MessageHandlers/MessageHandlerBase.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading.Tasks;
+using Bader.Edge.ModuleHost;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Logging;
+
+namespace AIT.Devices.MessageHandlers
+{
+    /// <summary>
+    /// This defines a message handler class which catches exceptions and logs them.
+    /// </summary>
+    public abstract class MessageHandlerBase : IMessageHandler
+    {
+        private readonly string _name;
+
+        protected ILogger Logger { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageHandlerBase"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        protected MessageHandlerBase(ILogger logger)
+        {
+            Logger = logger;
+            _name = GetType().FullName;
+        }
+
+        /// <inheritdoc />
+        async Task<MessageResponse> IMessageHandler.HandleMessageAsync(Message message)
+        {
+            try
+            {
+                return await HandleMessageAsync(message).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "An error occurred handling message in message handler {Name}", _name);
+                return Error();
+            }
+        }
+
+        /// <summary>
+        /// Returns a message response abandoned.
+        /// </summary>
+        protected MessageResponse Error() => MessageResponse.Abandoned;
+
+        /// <summary>
+        /// Handles a message from the cloud or another module. Exceptions get caught and automatically logged.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns>The message response.</returns>
+        protected abstract Task<MessageResponse> HandleMessageAsync(Message message);
+
+        /// <summary>
+        /// Returns a message response completed.
+        /// </summary>
+        protected MessageResponse Ok() => MessageResponse.Completed;
+    }
+}
---
+++ src/MethodHandlers/MethodHandlerBase.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Bader.Edge.ModuleHost;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Logging;
+
+namespace AIT.Devices.MethodHandlers
+{
+    /// <summary>
+    /// This defines a method handler class which catches exceptions and logs them.
+    /// </summary>
+    public abstract class MethodHandlerBase : IMethodHandler
+    {
+        private readonly string _name;
+
+        /// <summary>
+        /// Gets the logger.
+        /// </summary>
+        protected ILogger Logger { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MethodHandlerBase"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        protected MethodHandlerBase(ILogger logger)
+        {
+            Logger = logger;
+            _name = GetType().FullName;
+        }
+
+        /// <inheritdoc />
+        async Task<MethodResponse> IMethodHandler.HandleMethodAsync(MethodRequest methodRequest)
+        {
+            try
+            {
+                return await HandleMethodAsync(methodRequest).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "An error occurred handling method in method handler {Name}", _name);
+                return Error($"An error occurred handling method: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Returns a method response for a bad request.
+        /// </summary>
+        /// <param name="errorMessage">The error message.</param>
+        protected MethodResponse BadRequest(string errorMessage)
+            => new MethodResponse(Encoding.UTF8.GetBytes(errorMessage), (int)HttpStatusCode.BadRequest);
+
+        /// <summary>
+        /// Returns a method response for an error.
+        /// </summary>
+        /// <param name="errorMessage">The error message.</param>
+        protected MethodResponse Error(string errorMessage)
+            => new MethodResponse(Encoding.UTF8.GetBytes(errorMessage), (int)HttpStatusCode.InternalServerError);
+
+        /// <summary>
+        /// Handles a method from the cloud or another module.
+        /// </summary>
+        /// <param name="methodRequest">The method request.</param>
+        /// <returns>The method response.</returns>
+        protected abstract Task<MethodResponse> HandleMethodAsync(MethodRequest methodRequest);
+
+        /// <summary>
+        /// Returns a method response for a success.
+        /// </summary>
+        protected MethodResponse Ok() => new MethodResponse((int)HttpStatusCode.OK);
+    }
+}
